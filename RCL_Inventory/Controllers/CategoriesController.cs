a63a95a1bb6f5c7b26fbf346b4d3adf3777c3e4d
carlosmsbf <carlosmsbf@gmail.com>
2022-02-24 21:50:55 -0300

    Testing commit Carlos

--- RCL_Inventory/Controllers/CategoriesController.cs
+++ RCL_Inventory/Controllers/CategoriesController.cs
@@ -5,7 +5,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
-
+//Testing Carlos commit
 //Testing Royson commit
 
 namespace RCL_Inventory.Models
