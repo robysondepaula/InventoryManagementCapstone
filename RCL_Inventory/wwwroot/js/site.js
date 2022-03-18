96743bd258ce58c9fd641ac63bc84886405efa5c
robysondepaula <robysonwillyandepaula@gmail.com>
2022-03-18 01:03:03 -0400

    New Features Layout Display Home, Categories and Products

--- RCL_Inventory/wwwroot/js/site.js
+++ RCL_Inventory/wwwroot/js/site.js
@@ -1,4 +1,7 @@
-﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
-
-// Write your JavaScript code.
+﻿
+$(document).ready(function () {
+	$('.navbar-fostrap').click(function () {
+		$('.nav-fostrap').toggleClass('visible');
+		$('body').toggleClass('cover-bg');
+	});
+});
