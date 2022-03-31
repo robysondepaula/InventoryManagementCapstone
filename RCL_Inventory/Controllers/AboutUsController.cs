73cff6efdde29f74ead1588968ade48927f5c8e7
robysondepaula <robysonwillyandepaula@gmail.com>
2022-03-31 17:19:52 -0400

    Adjust Transaction name section, Create ContactUs, AboutUs, Help, controllers and pages.

---
+++ RCL_Inventory/Controllers/AboutUsController.cs
@@ -0,0 +1,26 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Identity;
+using RCL_Inventory.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.Extensions.Logging;
+
+
+namespace RCL_Inventory.Controllers
+{
+    public class AboutUsController : Controller
+    {
+        private readonly ILogger<AboutUsController> _logger;
+
+
+        public AboutUsController(ILogger<AboutUsController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ RCL_Inventory/Controllers/ContactUsControllercs.cs
@@ -0,0 +1,26 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Identity;
+using RCL_Inventory.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.Extensions.Logging;
+
+
+namespace RCL_Inventory.Controllers
+{
+    public class ContactUsController : Controller
+    {
+        private readonly ILogger<ContactUsController> _logger;
+
+
+        public ContactUsController(ILogger<ContactUsController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
